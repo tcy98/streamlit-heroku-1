1d88ae84bfbae850170507aee691244294fcdfb4
tcy98 <407277971ctang@gmail.com>
2022-04-12 18:54:42 -0500

    Update baseball.py

--- baseball.py
+++ baseball.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-batting = pd.read_csv("batting.csv")
+batting = pd.read_csv("Batting.csv")
 
 data = batting[["H","G"]].iloc[0:20]
 chart= st.bar_chart(data)
